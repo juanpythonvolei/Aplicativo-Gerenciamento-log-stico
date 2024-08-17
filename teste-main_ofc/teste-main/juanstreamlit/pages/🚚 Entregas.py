3bad48f3eb8e31bb143ede6a0390a11dc7c46697
juanpythonvolei <juanzsalca@outlook.com>
2024-08-17 12:58:41 -0300

    Update 🚚 Entregas.py

--- teste-main_ofc/teste-main/juanstreamlit/pages/🚚 Entregas.py
+++ teste-main_ofc/teste-main/juanstreamlit/pages/🚚 Entregas.py
@@ -29,7 +29,17 @@ try:
      checkbox_states = {}
      opcao_selecionada_data = st.selectbox("", lista_total,index=None,placeholder='Selecione uma data')
      if opcao_selecionada_data:
-          veiculo = st.selectbox('',lista_nomes,index = None,placeholder='Selecione um Veículo')
+          for item in dados:
+                                             
+                                             roteiro = dados[f'{item}']
+                                             for elemento in roteiro:
+                                                         nota = roteiro[f'{elemento}']
+                                                         data_emit = nota['Data de Emissão']
+                                                         if str(data_emit) == str(opcao_selecionada_data):
+                                                              if nota['Veículo'] != 'Indefinido': 
+                                                                 veiculo = st.selectbox('',lista_nomes,index = None,placeholder='Selecione um Veículo')
+                                                              else:
+                                                                   pass
           if veiculo:
           
                  try:
