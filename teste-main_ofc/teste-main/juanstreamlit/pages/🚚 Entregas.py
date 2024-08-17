466adfe364ec832a001a2eb97c6ee9ea962600c3
juanpythonvolei <juanzsalca@outlook.com>
2024-08-17 14:23:53 -0300

    Update 🚚 Entregas.py

--- teste-main_ofc/teste-main/juanstreamlit/pages/🚚 Entregas.py
+++ teste-main_ofc/teste-main/juanstreamlit/pages/🚚 Entregas.py
@@ -90,6 +90,7 @@ try:
                                status = 'Feito'
                                lista_conferida.append(status)
                                if len(lista_conferida) == len(lista_notas):
+                                           image = st.camera_input('Tire a foto do comprovante de recebimento do cliente')  
                                            requisicao_1 = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
                                            roteiro_1 = requisicao.json()
                                            dados_1 = roteiro_1['bancodedadosroteirooficial']
