9780e8aa0d9bd5592e8cb07f7c308f667e714b68
juanpythonvolei <juanzsalca@outlook.com>
2024-08-16 22:02:07 -0300

    Update 🛣️ Rotas e Suporte.py

--- teste-main_ofc/teste-main/juanstreamlit/pages/🛣️ Rotas e Suporte.py
+++ teste-main_ofc/teste-main/juanstreamlit/pages/🛣️ Rotas e Suporte.py
@@ -66,7 +66,7 @@ def escolha():
                                                                     base_url = "https://maps.googleapis.com/maps/api/geocode/json"
                                                                     params = {
                                                                                                 "address": address,
-                                                                                                "key": 'AIzaSyCMVv5_0c2dR16BM9r6ppgJ5sHXPD4MEc0'  # Substitua pela sua chave de API
+                                                                                                "key": st.secrets['firebase']['chave_api_googlemaps']  # Substitua pela sua chave de API
                                                                                             }
                                                             
                                                                     response = requests.get(base_url, params=params)
@@ -158,7 +158,7 @@ elif seletor == "rota de todas as notas":
                                                                                         base_url = "https://maps.googleapis.com/maps/api/geocode/json"
                                                                                         params = {
                                                                                                         "address": address,
-                                                                                                        "key": 'AIzaSyCMVv5_0c2dR16BM9r6ppgJ5sHXPD4MEc0'  # Substitua pela sua chave de API
+                                                                                                        "key": st.secrets['firebase']['chave_api_googlemaps']  # Substitua pela sua chave de API
                                                                                                     }
                                                             
                                                                                         response = requests.get(base_url, params=params)
@@ -188,7 +188,7 @@ elif seletor == "rota de todas as notas":
                                                                                         base_url = "https://maps.googleapis.com/maps/api/geocode/json"
                                                                                         params = {
                                                                                                         "address": address,
-                                                                                                        "key": 'AIzaSyCMVv5_0c2dR16BM9r6ppgJ5sHXPD4MEc0'  # Substitua pela sua chave de API
+                                                                                                        "key": st.secrets['firebase']['chave_api_googlemaps']  # Substitua pela sua chave de API
                                                                                                     }
                                                             
                                                                                         response = requests.get(base_url, params=params)
