8dff39288f5df6ff9accee60cea9f66ec1b3800f
juanpythonvolei <juanzsalca@outlook.com>
2024-08-17 15:56:02 -0300

    Update 🚚 Entregas.py

--- teste-main_ofc/teste-main/juanstreamlit/pages/🚚 Entregas.py
+++ teste-main_ofc/teste-main/juanstreamlit/pages/🚚 Entregas.py
@@ -77,10 +77,9 @@ try:
                                                                                    
                                                                                        # Usa o dicionário para controlar o estado da checkbox
                                                              
-                                                             checkbox_states[numero_nota] =  with col1: 
-                                                                 st.checkbox(f"Cliente: {cliente}. Nota: {numero_nota}. Volumes: {volumes}", key=numero_nota)
-                                                             with col2:
-                                                               image = st.camera_input(label = 'Se preferir, tire uma foto do comprovante de entrega')
+                                                             checkbox_states[numero_nota] =  st.checkbox(f"Cliente: {cliente}. Nota: {numero_nota}. Volumes: {volumes}", key=numero_nota)
+                                                            
+                                                             image = st.camera_input(label = 'Se preferir, tire uma foto do comprovante de entrega') 
                                                            else:
                    
                                                              st.success('Entrega Completa')
